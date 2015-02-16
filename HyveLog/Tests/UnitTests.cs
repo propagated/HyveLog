91b1f6619a66188b054aeaad18360695d2790d09
pwilliams <tehpete@gmail.com>
2015-02-15 19:14:36 -0500

    test refactor in preparation for travis still need to figure out how to handle file pathing in a mono environment

--- HyveLog/Tests/UnitTests.cs
+++ HyveLog/Tests/UnitTests.cs
@@ -17,11 +17,30 @@ namespace HyveLog.UnitTests
     internal class TestHyveLog
     {
         const String errorMessage = "This is a test error message.";
-        
+
+        const String relativePath = "errors\\errorlog.txt";
+        String defaultPath;
+        const String absolutePath = @"C:\Errors\errorlog.txt";
+        const String logFile = "errorlog.txt";
+
         [TestFixtureSetUp, Description("")]
         public void InitTestFixture()
         {
-            //fires once at load before any test
+            //fires once at load before tests start
+            defaultPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Errors\\ErrorLog.txt");
+            if (Directory.Exists(Path.GetDirectoryName(defaultPath)))
+            {
+                File.Delete(defaultPath);
+            }
+            if (Directory.Exists(Path.GetDirectoryName(relativePath)))
+            {
+                Directory.Delete(Path.GetDirectoryName(relativePath), true);
+            }
+            if (Directory.Exists(Path.GetDirectoryName(absolutePath)))
+            {
+                File.Delete(absolutePath);
+            }
+            File.Delete(logFile);
         }
         [TestFixtureTearDown]
         public void EndAllTests()
@@ -42,50 +61,38 @@ namespace HyveLog.UnitTests
         [Test, Description(@"Write to File, no path passed. errors\errorlog.txt will be created and written to in user\appdata\local folder.")]
         public void TestConstructorNoParams()
         {
-            var defaultPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Errors\\ErrorLog.txt");
-            if (File.Exists(defaultPath))
-            {
-                File.Delete(defaultPath);
-            }
             var logger = new Logger(Logger.LogTarget.File);
             logger.Log(errorMessage);
             Assert.IsTrue(File.Exists(logger.LogFileFullPath));
-            Assert.AreEqual(errorMessage, File.ReadAllText(logger.LogFileFullPath));
+            var writtenMessage = File.ReadAllText(logger.LogFileFullPath).Split(new String[] {Environment.NewLine}, StringSplitOptions.None)[0];
+            Assert.AreEqual(errorMessage, writtenMessage);
         }
         [Test, Description("Write to File valid path.")]
         public void TestConstructorAbsolutePath()
         {
-            var absolutePath = @"C:\Errors\errorlog.txt";
-            if (!Directory.Exists(Path.GetDirectoryName(absolutePath)))
-            {
-                Directory.CreateDirectory(Path.GetDirectoryName(absolutePath));
-            }
             var logger = new Logger(Logger.LogTarget.File, absolutePath);
             logger.Log(errorMessage);
             Assert.IsTrue(File.Exists(logger.LogFileFullPath));
-            Assert.AreEqual(errorMessage, File.ReadAllText(logger.LogFileFullPath));
+            var writtenMessage = File.ReadAllText(logger.LogFileFullPath).Split(new String[] { Environment.NewLine }, StringSplitOptions.None)[0];
+            Assert.AreEqual(errorMessage, writtenMessage);
         }
         [Test, Description("Write to File with relative path to calling directory.")]
         public void TestConstructorRelativeLogFilePath()
         {
-            var logger = new Logger(Logger.LogTarget.File, "errorlog.txt");
+            var logger = new Logger(Logger.LogTarget.File, logFile);
             logger.Log(errorMessage);
             Assert.IsTrue(File.Exists(logger.LogFileFullPath));
-            Assert.AreEqual(errorMessage, File.ReadAllText(logger.LogFileFullPath));
+            var writtenMessage = File.ReadAllText(logger.LogFileFullPath).Split(new String[] { Environment.NewLine }, StringSplitOptions.None)[0];
+            Assert.AreEqual(errorMessage, writtenMessage);
         }
         [Test, Description("Write to File path with with directory that doesn't exist, directory will be created.")]
         public void TestCreateDirectoryBeforeWritingFile()
         {
-            var relativePath = "errors\\errorlog.txt";
-            if (Directory.Exists(Path.GetDirectoryName(relativePath)))
-            {
-                Directory.Delete(Path.GetDirectoryName(relativePath));
-            }
-
             var logger = new Logger(Logger.LogTarget.File, relativePath);
             logger.Log(errorMessage);
             Assert.IsTrue(File.Exists(logger.LogFileFullPath));
-            Assert.AreEqual(errorMessage, File.ReadAllText(logger.LogFileFullPath));
+            var writtenMessage = File.ReadAllText(logger.LogFileFullPath).Split(new String[] { Environment.NewLine }, StringSplitOptions.None)[0];
+            Assert.AreEqual(errorMessage, writtenMessage);
         }
         [Test, Description("Write to console")]
         public void TestWritingToConsole()
