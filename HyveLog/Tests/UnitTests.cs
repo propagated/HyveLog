bb3f13ac6f8cf318b1337405eb0bce1f928a539a
pwilliams <tehpete@gmail.com>
2015-02-21 21:31:15 -0500

    mono console output test refactor to only test both mode giving up on console reading in mono for now.

--- HyveLog/Tests/UnitTests.cs
+++ HyveLog/Tests/UnitTests.cs
@@ -17,21 +17,31 @@ namespace HyveLog.UnitTests
     internal class TestHyveLog
     {
         const String errorMessage = "This is a test error message.";
-        const String logFile = "errorlog.txt";
-        const String consoleLogFile = "consolelog.txt";
-        String relativePath = Path.Combine("errors", logFile);
-        String defaultPath;
-        String absolutePath;
+
+        const String relativePath = @"errors\errorlog.txt";
         
+        String defaultPath;
+        //String absolutePath = @"C:\Errors\errorlog.txt";
+        String absolutePath;
+        const String logFile = "errorlog.txt";
+        const String bothPath = "bothLog.txt";
 
         [TestFixtureSetUp, Description("")]
         public void InitTestFixture()
         {
             //set default relative path, use to set absolute path
-            defaultPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Errors", "ErrorLog.txt");
-            absolutePath = Path.Combine(Path.GetDirectoryName(defaultPath),"aboslutePath", "errorLog.txt");
+            defaultPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Errors\\ErrorLog.txt");
+            //should be c:\\ or / in mono
+            if (Path.GetPathRoot(defaultPath).Equals('/'))
+            {
+                absolutePath = Path.Combine(Path.GetPathRoot(defaultPath), "errors/errorlog.txt");
+            }
+            else
+            {
+                absolutePath = Path.Combine(Path.GetPathRoot(defaultPath), relativePath);
+            }
 
-            //clear out any test artifacts
+            //clear out test artifacts
             if (Directory.Exists(Path.GetDirectoryName(defaultPath)))
             {
                 File.Delete(defaultPath);
@@ -42,18 +52,15 @@ namespace HyveLog.UnitTests
             }
             if (Directory.Exists(Path.GetDirectoryName(absolutePath)))
             {
-                Directory.Delete(Path.GetDirectoryName(absolutePath), true);
+                File.Delete(absolutePath);
             }
             File.Delete(logFile);
-            File.Delete(consoleLogFile);
+            File.Delete(bothPath);
         }
         [TestFixtureTearDown]
         public void EndAllTests()
         {
             //fires after all tests are complete
-            StreamWriter standardOutput = new StreamWriter(Console.OpenStandardOutput());
-            standardOutput.AutoFlush = true;
-            Console.SetOut(standardOutput);
         }
         [SetUp]
         public void InitTest()
@@ -102,35 +109,14 @@ namespace HyveLog.UnitTests
             var writtenMessage = File.ReadAllText(logger.LogFileFullPath).Split(new String[] { Environment.NewLine }, StringSplitOptions.None)[0];
             Assert.AreEqual(errorMessage, writtenMessage);
         }
-        [Test, Description("Write to console")]
-        public void TestWritingToConsole()
+        [Test, Description("Write to console and a file at the same time.")]
+        public void TestWritingToConsoleAndFile()
         {
-            var logger = new Logger();
-            FileStream fs = new FileStream("consoleError.txt", FileMode.Create);
-            StreamWriter sw = new StreamWriter(fs);
-
-            Console.SetOut(sw);
+            var logger = new Logger(Logger.LogTarget.Both, "logBoth.txt");
             logger.Log(errorMessage);
-            sw.Flush();
-            fs.Flush();
-            fs.Close();
-            var writtenMessage = File.ReadAllText("consoleError.txt").Split(new String[] { Environment.NewLine }, StringSplitOptions.None)[0];
+            Assert.IsTrue(File.Exists(logger.LogFileFullPath));
+            var writtenMessage = File.ReadAllText(logger.LogFileFullPath).Split(new String[] { Environment.NewLine }, StringSplitOptions.None)[0];
             Assert.AreEqual(errorMessage, writtenMessage);
-
-            //TextWriter stdout = Console.Out;
-            //StringWriter sw = new StringWriter();
-            //Console.SetOut(sw);
-            //try
-            //{
-            //    logger.Log(errorMessage);
-            //}
-            //finally
-            //{
-            //    Console.SetOut(stdout);
-            //    string output = sw.ToString();
-            //    sw.Close();
-            //    Assert.AreEqual(errorMessage + Environment.NewLine, output);
-            //}
         }
     }
 }
